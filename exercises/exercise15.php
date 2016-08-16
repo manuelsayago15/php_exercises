3f285a230e3792165dcb02ccd3d0aeb244422540
Manuel Sayago <Manuel Sayago@kentron.com.ve>
2016-08-16 16:12:21 -0400

    Ejercicio 15 casi terminado, falta validar las horas

--- exercises/exercise15.php
+++ exercises/exercise15.php
@@ -6,19 +6,22 @@
 	</head>
 	<body>
 		<h2>PHP Exercises</h2>
-		<h3>Exercise 14</h3>
+		<h3>Exercise 15</h3>
+		<p>15. Un obrero necesita calcular su salario semanal, el cual se obtiene de la sig.     manera:
+		<ul>
+			<li>Si trabaja 40 horas o menos se le paga $16 por hora.</li>
+			<li>Si trabaja más de 40 horas se le paga $16 por cada una de las primeras 40 horas y $20 por cada hora extra.</li>
+		</ul>
+    Validar los datos de entrada.
+</p>
 		<form name="exer14" action="#" method="post" id="constant" name="constant">
 			<div>
-				<label for="text">Grade 1: </label>
-				<input type="number" name="grade1" placeholder="Type grade 1" >
-			</div>
+				<label for="text">Hours: &nbsp &nbsp &nbsp &nbsp</label>
+				<input type="text" name="hours" placeholder="Enter hours from 0 to 40" >
+			</div><br>
 			<div>
-				<label for="text">Grade 2: </label>
-				<input type="number" name="grade2" placeholder="Type grade 2" >
-			</div>
-			<div>
-				<label for="text">Grade 3: </label>
-				<input type="number" name="grade3" placeholder="Type grade 3" >
+				<label for="text">Extra hours: </label>
+				<input type="text" name="extra" placeholder="Enter hours from 0 to 40" >
 			</div>
 			<div>
 				<input type="submit" name="button" id="sending" value="Send">
@@ -27,7 +30,42 @@
 
 		<?php
 			if (isset($_POST['button'])) {
-				# code...
+				$hours = $_POST['hours'];
+				$extra = $_POST['extra'];
+				$salary;
+				$errors = array();
+
+				if (empty($_POST['hours'])) {
+					echo "No se aceptan campos vacíos, por favor completar todos los campos.";
+				}elseif (is_numeric($hours)==FALSE || is_numeric($extra)==FALSE) {
+					if (is_numeric($hours)==FALSE) {
+						echo "Error! El campo de horas no puede tener letras." . "<br>";
+					}
+					if (is_numeric($extra)==TRUE) {
+						echo "Error! El campo de horas extra no puede tener letras." . "<br>";
+					}
+					//var_dump($hours);
+				}elseif ($hours<0 || $hours>40 || $extra<0 || $extra>25) {
+					if ($hours<0 || $hours>40) {
+						echo "Ingrese un número entre 0-40 para representar las horas trabajadas por el obrero." . "<br>";
+					}
+					if ($extra<0 || $extra>25) {
+						echo "Ingrese un número entre 0-25 para representar las horas extras trabajadas por el obrero." . "<br>";
+					}
+				}else{
+					if ($hours<=40) {
+						$salary = $hours*16;
+						echo "El salario semanal del obrero es de " . "$" . $salary;
+					}elseif ($hours>40) {
+						$extra = 40-$extra;
+						$salary = ($hours*16)+($extra*20);
+						echo "El salario semanal del obrero es de " . "$" . $salary;
+					}
+				}
+
+
+
+
 			}
 
 
